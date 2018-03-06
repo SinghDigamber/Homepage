118ff51a4408ace7f9bdb24849d8a01b6edd7b07
Oleh Krupko <OlehKrupko@gmail.com>
2018-03-06 13:00:37 +0200

    Production preparation

---
+++ Homepage/installation.sh
@@ -0,0 +1,11 @@
+ssh pi@192.168.1.201
+python3 -m pip install -U --force-reinstall pip
+python3 -m pip install --upgrade pip
+sudo python3 -m pip install django~=2.0.2
+
+sudo python3 -m pip install beautifulsoup4
+sudo python3 -m pip install requests
+sudo python3 -m pip install feedparser
+
+
+git clone https://github.com/OlehKrupko/Homepage
