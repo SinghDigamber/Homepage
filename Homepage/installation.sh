ae39ad08d1948d90aab1303d180cbf5d593a1ef5
Oleh Krupko <OlehKrupko@gmail.com>
2018-03-06 22:15:04 +0200

    fix

--- Homepage/installation.sh
+++ Homepage/installation.sh
@@ -7,5 +7,6 @@ sudo python3 -m pip install beautifulsoup4
 sudo python3 -m pip install requests
 sudo python3 -m pip install feedparser
 
-
+rm -rf Homepage
 git clone https://github.com/OlehKrupko/Homepage
+python3 Homepage/manage.py runserver 192.168.1.201:8000
