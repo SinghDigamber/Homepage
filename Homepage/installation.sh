699ffb2f3dc43ce851051039f2aefe7dd4d85a3a
Oleh Krupko <OlehKrupko@gmail.com>
2018-03-08 15:48:43 +0200

    fix

--- Homepage/installation.sh
+++ Homepage/installation.sh
@@ -9,4 +9,4 @@ sudo python3 -m pip install feedparser
 
 rm -rf ~/Homepage
 git clone https://github.com/OlehKrupko/Homepage
-python3 ~/Homepage/manage.py runserver 192.168.1.201:80
+python3 ~/Homepage/manage.py runserver 192.168.1.201:8000
