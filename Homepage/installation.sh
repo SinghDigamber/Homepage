c4d2c4753a0f152b11af76360d972306b5c30497
Oleh Krupko <OlehKrupko@gmail.com>
2018-03-09 14:12:25 +0200

    style review: iOS menu-top fix

--- Homepage/installation.sh
+++ Homepage/installation.sh
@@ -6,8 +6,11 @@ sudo python3 -m pip install django~=2.0.2
 sudo python3 -m pip install requests # downloads html
 sudo python3 -m pip install beautifulsoup4 # used to parse html
 sudo python3 -m pip install collective.soupstrainer # allows to parse part of the page
+sudo apt-get update
+sudo apt-get -f install --fix-missing
 sudo python3 -m pip install lxml # html parser
 sudo apt-get install libxml2-dev libxslt1-dev # requied for lxml
+sudo python3 -m pip install lxml # html parser
 sudo python3 -m pip install feedparser # rss parser
 
 #!/bin/sh
