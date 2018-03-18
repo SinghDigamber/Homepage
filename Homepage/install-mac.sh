e3a84aacfbd4c5db61a5b0098b7c417fa46cbed2
Oleh Krupko <oleh.krupko@gmail.com>
2018-03-18 16:36:21 +0200

    Set up Mac instructions added

---
+++ Homepage/install-mac.sh
@@ -0,0 +1,11 @@
+# install Xcode with command line tools OR only tools with command:
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install python3
+# PyCharm install:
+Django
+beautifulsoup4
+collective.soupstrainer
+feedparser
+lxml
+requests
