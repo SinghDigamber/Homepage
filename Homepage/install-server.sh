72ed3b2cd3566c08214afb1d3afbc59ef6f9605f
Oleh Krupko <oleh.krupko@gmail.com>
2018-03-19 03:37:02 +0200

    Fix

--- Homepage/install-server.sh
+++ Homepage/install-server.sh
@@ -14,14 +14,40 @@ sudo python3 -m pip install lxml # html parser
 sudo python3 -m pip install feedparser # rss parser
 
 #!/bin/sh
-cd ~
-rm -rf ~/Homepage
-git clone https://github.com/OlehKrupko/Homepage
-python3 ~/Homepage/manage.py runserver 192.168.1.201:8000
-cd ~
+
+### BEGIN INIT INFO
+# Provides:          olehkrupko.com.django
+# Required-Start:
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      1 0 6
+# Short-Description: launches Django demon
+### END INIT INFO
+
+rm -rf /home/pi/Homepage
+git clone https://github.com/OlehKrupko/Homepage /home/pi/Homepage
+python3 /home/pi/Homepage/manage.py runserver 192.168.1.201:8000
+
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          olehkrupko.com.django-cacher
+# Required-Start:
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      1 0 6
+# Short-Description: launches Django-cacher demon
+### END INIT INFO
+
+while sleep 600; do curl "http://home.olehkrupko.com/bookUpdates/cache"; done
 
 sudo nano /etc/init.d/olehkrupko.com.django
+sudo nano /etc/init.d/olehkrupko.com.django-cacher
+
 sudo chmod ugo+x /etc/init.d/olehkrupko.com.django
+sudo chmod ugo+x /etc/init.d/olehkrupko.com.django-cacher
 
 python3 ~/Homepage/manage.py migrate
 python3 ~/Homepage/manage.py createsuperuser
+
+ps auxw | grep runserver
