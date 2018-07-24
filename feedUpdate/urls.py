12ad9cd3328280e848e7752688a60087474c7fc6
Oleh Krupko <oleh.krupko@gmail.com>
2018-07-25 00:08:02 +0300

    feedUpdate/urls.py: _ symbol support added

--- feedUpdate/urls.py
+++ feedUpdate/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^more', views.feedUpdateIndexMoreView.as_view(), name="index/all"),
     url(r'^cache$', views.feedUpdateCacheView.as_view(), name="index/cache"),
     url(r'^force$', views.feedUpdateForceIndexView.as_view(), name="index/force"),
-    url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|+])+)$', views.feedUpdateIndexView.as_view(), name="feed"),
-    url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|+])+)/force$', views.feedUpdateForceIndexView.as_view(), name="feed/force"),
+    url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|_|+])+)$', views.feedUpdateIndexView.as_view(), name="feed"),
+    url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|_|+])+)/force$', views.feedUpdateForceIndexView.as_view(), name="feed/force"),
 ]
