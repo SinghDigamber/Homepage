0a71d5437ad3aeb57e250f9ab32024ba40649413
Oleh Krupko <oleh.krupko@gmail.com>
2018-08-10 11:56:07 +0300

    feedUpdate: caching updated

--- feedUpdate/management/commands/cache_clear.py
+++ feedUpdate/management/commands/cache_clear.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from feedUpdate.models import feedUpdate
 
 class Command(BaseCommand):
-    help = 'removes all information from database'
+    help = 'removes all cache from DB'
 
     def handle(self, *args, **options):
         feedUpdate.objects.all().delete()
