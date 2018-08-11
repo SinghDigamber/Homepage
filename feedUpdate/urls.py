c812a9003c50544c4c70cfcddc827f1b769a3848
Oleh Krupko <oleh.krupko@gmail.com>
2018-08-11 12:21:45 +0300

    feedUpdate: fix

--- feedUpdate/urls.py
+++ feedUpdate/urls.py
@@ -6,6 +6,6 @@ app_name = "feedUpdate"
 urlpatterns = [
     url(r'^$', views.feedUpdateIndexView.as_view(), name="index"),
     url(r'^feeds/$', views.feedIndexView.as_view(), name="feeds"),  # return list of ffeds with links to them
-    url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|_|+])*)(\/)?(?P<force>(\/force))?$',  # add limit option as well
+    url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|_|+|—])*)(\/)?(?P<force>(\/force))?$',  # add limit option as well
         views.feedUpdateIndexView.as_view(), name="feed"),
 ]
