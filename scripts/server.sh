063ef8639f497e9d291fa3bfdf6ae362a402e132
Oleh Krupko <oleh.krupko@gmail.com>
2018-09-26 02:07:05 +0300

    scripts: fixes

--- scripts/server.sh
+++ scripts/server.sh
@@ -18,18 +18,19 @@ sudo python3 -m pip install feedparser # rss parser
 rm -rf /home/pi/Projects/Homepage
 git clone https://github.com/OlehKrupko/Homepage /home/pi/Projects/Homepage
 
-sudo cp ~/Projects/Homepage/scripts/homepage.server /etc/init.d/
-sudo cp ~/Projects/Homepage/scripts/homepage.watcher /etc/init.d/
+# sudo cp ~/Projects/Homepage/scripts/homepage.server /etc/init.d/
+# sudo cp ~/Projects/Homepage/scripts/homepage.watcher /etc/init.d/
 # sudo chmod ugo+x /etc/init.d/server-django.sh
 # sudo chmod ugo+x /etc/init.d/server-django-cacher.sh
 
 
 ps auxw | grep runserver
 
-cp /home/pi/Projects/Homepage/scripts/homepage.watcher.service /lib/systemd/system/homepage.watcher.service
-cp /home/pi/Projects/Homepage/scripts/homepage.server.service /lib/systemd/system/homepage.server.service
+sudo cp /home/pi/Projects/Homepage/scripts/homepage.cacheUpdate.service /lib/systemd/system/homepage.cacheUpdate.service
+sudo cp /home/pi/Projects/Homepage/scripts/homepage.cacheUpdate.timer /lib/systemd/system/homepage.cacheUpdate.timer
+sudo cp /home/pi/Projects/Homepage/scripts/homepage.server.service /lib/systemd/system/homepage.server.service
 
-nano /tmp/homepage.watcher.service.result
+nano /tmp/homepage.cacheUpdate.service.result
 
 
 sudo systemctl status homepage.server.service
@@ -39,9 +40,9 @@ sudo systemctl start homepage.server.service
 
 
 
-sudo systemctl status homepage.watcher.service
-sudo systemctl enable homepage.watcher.service
-sudo systemctl start homepage.watcher.service
+sudo systemctl status homepage.cacheUpdate.service
+sudo systemctl enable homepage.cacheUpdate.service
+sudo systemctl start homepage.cacheUpdate.service
 
 
 
