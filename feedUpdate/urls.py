4fcd75ea8d2436967fa61320fc1d86e4b91c4c2a
Oleh Krupko <oleh.krupko@gmail.com>
2018-12-14 12:38:04 +0200

    feedUpdate/urls: typo

--- feedUpdate/urls.py
+++ feedUpdate/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "feedUpdate"
 urlpatterns = [
     url(r'^$', views.feedUpdateIndexView.as_view(), name="index"),
-    url(r'^feeds/$', views.feedIndexView.as_view(), name="feeds"),  # return list of ffeds with links to them
     url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|_|+|—])*)(\/)?(?P<force>(\/force))?$',  # add limit option as well
+    url(r'^feeds/$', views.feedIndexView.as_view(), name="feeds"),  # return list of feeds with links to them
         views.feedUpdateIndexView.as_view(), name="feed"),
 ]
