87ac25155c8910978beeed9303f5622ae223583c
Oleh Krupko <oleh.krupko@gmail.com>
2019-02-25 20:29:45 +0200

    urls: main page redirect now fixed

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # main
-    url(r'^/$', RedirectView.as_view(url='/feedUpdate/', permanent=False), name="Dashboard"),
+    url(r'^$', RedirectView.as_view(url='/feedUpdate/', permanent=False), name="Dashboard"),
 
     # modules
     url(r'^feedUpdate/', include('feedUpdate.urls', namespace="feedUpdate")),
