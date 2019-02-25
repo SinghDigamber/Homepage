85578adeb67924a1f7bce3b061f341a1becdf5de
Oleh Krupko <oleh.krupko@gmail.com>
2019-02-25 21:09:04 +0200

    fU/admin: added admin filtering

--- feedUpdate/admin.py
+++ feedUpdate/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import feedUpdate
+from .models import feed, feedUpdate
 
 # Register your models here.
 class feedUpdateAdmin(admin.ModelAdmin):
     list_display = ('name', 'datetime', 'title', 'href')
-    # list_filter = ('inIndex', 'datetime')
-
+    list_filter = ('title',)
 admin.site.register(feedUpdate, feedUpdateAdmin)
+
+
+class feedAdmin(admin.ModelAdmin):
+    list_display = ('title', 'title_full', 'emojis', 'filter', 'delay', 'href', 'href_title')
+    list_filter = ('emojis',)
+admin.site.register(feed, feedAdmin)
