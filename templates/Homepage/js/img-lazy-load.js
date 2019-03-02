6cace48f1618aca4b820fd65aeb06f5155d7a8db
Oleh Krupko <oleh.krupko@gmail.com>
2019-03-02 08:04:23 +0200

    Dash: lazy-load fixed

--- templates/Homepage/js/img-lazy-load.js
+++ templates/Homepage/js/img-lazy-load.js
@@ -1,25 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages = document.querySelectorAll("img.lazy");
-  var lazyloadThrottleTimeout;
+    var lazyloadImages = document.querySelectorAll("img.lazy-load");
+    var lazyloadThrottleTimeout;
 
-  function lazyload () {
+    function lazyload () {
     if(lazyloadThrottleTimeout) {
-      clearTimeout(lazyloadThrottleTimeout);
+        clearTimeout(lazyloadThrottleTimeout);
     }
 
     lazyloadThrottleTimeout = setTimeout(function() {
         var scrollTop = window.pageYOffset;
         lazyloadImages.forEach(function(img) {
             if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
+                img.src = img.dataset.src;
+                img.classList.remove('lazy-load');
             }
         });
-        if(lazyloadImages.length == 0) {
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
-        }
     }, 20);
   }
 });
