46da20ab060c8e06958f1dfe798ff62bc769a17d
Oleh Krupko <oleh.krupko@gmail.com>
2019-03-02 08:17:41 +0200

    Dash: img-lazy-load seems to fixed this time

--- templates/Homepage/js/img-lazy-load.js
+++ templates/Homepage/js/img-lazy-load.js
@@ -1,20 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var lazyloadImages = document.querySelectorAll("img.lazy-load");
-    var lazyloadThrottleTimeout;
+  var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadThrottleTimeout;
 
-    function lazyload () {
+  function lazyload () {
     if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
+      clearTimeout(lazyloadThrottleTimeout);
     }
 
     lazyloadThrottleTimeout = setTimeout(function() {
         var scrollTop = window.pageYOffset;
         lazyloadImages.forEach(function(img) {
             if(img.offsetTop < (window.innerHeight + scrollTop)) {
-                img.src = img.dataset.src;
-                img.classList.remove('lazy-load');
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
             }
         });
+        // if(lazyloadImages.length == 0) {
+          // document.removeEventListener("scroll", lazyload);
+          // window.removeEventListener("resize", lazyload);
+          // window.removeEventListener("orientationChange", lazyload);
+        // }
     }, 20);
   }
+
+  lazyload()
+
+  // document.addEventListener("scroll", lazyload);
+  // window.addEventListener("resize", lazyload);
+  // window.addEventListener("orientationChange", lazyload);
 });
