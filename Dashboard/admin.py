3f97599e6e2babe67dc2ab99c69a40a812cb08f3
Oleh Krupko <oleh.krupko@gmail.com>
2019-03-25 23:27:39 +0200

    Dash: PlanetaKino admin

---
+++ Dashboard/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import PlanetaKino
+
+
+class PlanetaKinoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'posterIMG', 'href', 'date', 'inTheater')
+    list_filter = ('inTheater',)
+admin.site.register(PlanetaKino, PlanetaKinoAdmin)
