d5ffcb0b9b08d2001fcf6851f2dca753136d5392
Oleh Krupko <oleh.krupko@gmail.com>
2019-03-27 03:41:25 +0200

    fU/urls: a few renames

--- feedUpdate/urls.py
+++ feedUpdate/urls.py
@@ -5,7 +5,8 @@ app_name = "feedUpdate"
 urlpatterns = [
     url(r'^feeds$', views.feedIndexView.as_view(), name="feeds"),  # list feeds
     url(r'^myActivity', views.myActivityView.as_view(), name="myActivity"),  # list feeds
+
     url(r'^(?P<mode>(|index|force))$', views.feedUpdateIndexView.as_view(), name="index"),  # main fU feed with modes
     url(r'^(?P<feeds>([0-9|а-я|А-Я|a-z|A-Z|_|+|—])*)/(?P<mode>(|index|force))?$',
-        views.feedUpdateIndexView.as_view(), name="feed"),  # view separate feeds
+        views.feedUpdateIndexView.as_view(), name="feed"),  # view separate feeds with filters
 ]
