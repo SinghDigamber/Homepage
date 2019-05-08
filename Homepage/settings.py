64ba4c31940dafdc1d50871b32c97fbf5b390f9b
Oleh Krupko <oleh.krupko@gmail.com>
2019-05-08 23:54:01 +0300

    DB: migration to MySQL

--- Homepage/settings.py
+++ Homepage/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'Homepage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Homepage',
+        'USER': 'pi',
+        'PASSWORD': 'barracuda',
+        'HOST': '192.168.1.201',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    },
 }
 
 
