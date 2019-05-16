31301b3be2cfee7f63555cfba07611d9d22c5974
Oleh Krupko <oleh.krupko@gmail.com>
2019-05-17 01:59:47 +0300

    DB: PostgreSQL migration #1

--- Homepage/settings.py
+++ Homepage/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'Homepage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homepage_django',
+        'USER': 'pi',
+        'PASSWORD': 'bali4dead',
+        'HOST': '192.168.1.201',
+        'PORT': '5432',
     }
 }
 
