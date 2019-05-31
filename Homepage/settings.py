5aa61eea34c1cf87bd212e5fe3580be7d30c9544
Oleh Krupko <oleh.krupko@gmail.com>
2019-05-31 21:31:00 +0300

    DB: SQLite settings can be easily reverted

--- Homepage/settings.py
+++ Homepage/settings.py
@@ -84,6 +84,8 @@ WSGI_APPLICATION = 'Homepage.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'homepage_django',
         'USER': 'pi',
