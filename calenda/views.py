1625b519538b0abb9e9b889ef2e20b02f45c1868
Oleh Krupko <oleh.krupko@gmail.com>
2019-07-06 15:37:58 +0300

    calenda: listView ready

--- calenda/views.py
+++ calenda/views.py
@@ -8,12 +8,23 @@ class eventListView(ListView):
     context_object_name = "fromView"
 
     def get_queryset(self):
-        # items_limit = 4200
+        event_list = {}
 
-        event_list = event.objects.all()
         # today
-        # this week
-        # each month separately
+        event_list['today'] = event.objects.filter(start__date=date.today(), end__gte=datetime.now())
+
+        # tomorrow
+        event_list['tomorrow'] = event.objects.filter(start__date=date.today() + timedelta(days=1))
+
+        # until end of the week
+        after_tomorrow = date.today() + timedelta(days=2)
+
+        days_ahead = 6 - date.today().weekday()  # 0 = Monday, 1=Tuesday, 2=Wednesday...
+        if days_ahead <= 0: # Target day already happened this week
+            days_ahead += 7
+        sunday = date.today() + timedelta(days=days_ahead)
+
+        event_list['this_week'] = event.objects.filter(start__date__gt=after_tomorrow, end__date__lte=sunday)
 
         return {
             'page_title': "cписок",
