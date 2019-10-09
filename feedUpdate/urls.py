887e53d0f9b05031cdc6e326f2093fec4ad2e7e5
Oleh Krupko <oleh.krupko@gmail.com>
2019-10-09 23:33:58 +0300

    fU: urls updated

--- feedUpdate/urls.py
+++ feedUpdate/urls.py
@@ -3,16 +3,15 @@ from . import views
 
 app_name = "feedUpdate"
 urlpatterns = [
-    url(r'^$', views.feedUpdateIndexView.as_view(), name="main"),  # main fU feed with modes
+    url(r'^(?P<mode>())$', views.feedUpdateIndexView.as_view(), name="main"),  # main fU feed with modes
     url(r'^index:(?P<mode>(index|force|more))$', views.feedUpdateIndexView.as_view(), name="index"),  # main fU feed with modes
     url(r'^other:(?P<mode>(index|force|more))', views.otherView.as_view(), name="other"),  # list other
 
     # pages
     url(r'^feeds$', views.feedIndexView.as_view(), name="feeds"),  # list feeds
     url(r'^feedsAll$', views.feedIndexFullView.as_view(), name="feedsAll"),  # list all feeds
-    url(r'^tests$', views.feedTestsView.as_view(), name="tests"),
-    url(r'^rss$', views.feedUpdateFeed(), name="rss"),
+    url(r'^rss$', views.feedUpdateFeed(), name="rss"),  # RSS feed
+    url(r'^tests$', views.feedTestsView.as_view(), name="tests"),  # testing page
     
-    url(r'^custom?feeds=(?P<feeds>([0-9|а-я|ё|А-Я|Ё|a-z|A-Z|_|+|—])*)/(?P<mode>(|index|force|more))?$',
-        views.feedUpdateIndexView.as_view(), name="feed"),  # view separate feeds with filters
+    url(r'^feed/(?P<feeds>([0-9|а-я|ё|А-Я|Ё|a-z|A-Z|_|+|—])*):(?P<mode>(index|force|more))?$', views.feedUpdateIndexView.as_view(), name="feed"),  # view custom feed
 ]
