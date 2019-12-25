8196efc218f6a732cc6f0562ea00f19ce6524665
Oleh Krupko <oleh.krupko@qpard.com>
2019-12-26 01:57:20 +0200

    fU: caching is now using multiprocessing

--- feedUpdate/management/commands/cacheFeedUpdate.py
+++ feedUpdate/management/commands/cacheFeedUpdate.py
@@ -6,26 +6,70 @@ from datetime import datetime
 from tqdm import tqdm
 from random import shuffle
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 class Command(BaseCommand):
     help = 'updates caches in DB'
 
     def add_arguments(self, parser):
-        parser.add_argument('--log', action='store_true')
-        parser.add_argument('--logEach', action='store_true')
-        parser.add_argument('--logBar', action='store_true')
-
+        # parsing mode
+        parser.add_argument('--parseFeeds', action='store_true')
+        parser.add_argument('--parseUpdates', action='store_true')
+        
+        # parsing modifiers
         parser.add_argument('--shuffle', action='store_true')
         parser.add_argument('--useProxy', action='store_true')
         parser.add_argument('--printEmpty', action='store_true')
 
-        parser.add_argument('--parseFeeds', action='store_true')
-        parser.add_argument('--parseAll', action='store_true')
-        parser.add_argument('--parseIndex', action='store_true')
-        parser.add_argument('--parseNow', action='store_true')
+        # logging options
+        parser.add_argument('--log', action='store_true')
+        parser.add_argument('--logEach', action='store_true')
+        parser.add_argument('--logBar', action='store_true')
+
+    def print_feed(amount, time, title):
+        print(f"┣ added {title} x{str(amount)} in {str(time)}s")
+
+    def print_total(amount, time):
+        print(f"└──── added {str(amount)} in {str(time)}s")
+
+    def process_feed(current_feed):
+        # cycle preparation
+        cycle_time = time.time()
+        cycle_items = 0
+        cycle_items_total = 0
+
+        # PARSING HERE
+        feedUpdate_list = current_feed.parse()
+        feedUpdate_list = reversed(feedUpdate_list)
+
+        # checking if feed is new: new feeds use real datetime
+        new_feed = True if len(feedUpdate.objects.filter(title=current_feed.title)) == 0 else False
+
+        for each in feedUpdate_list:
+            # checking if href is cached
+            cached = feedUpdate.objects.filter(href=each.href).exists()
+            
+            cycle_items_total += 1
+            if not cached:
+                if new_feed:
+                    each.datetime = datetime.now()
+                
+                each.save()
+                cycle_items += 1
+
+        cycle_time = time.time() - cycle_time
+        cycle_time = round(cycle_time, 2)
+
+        cycle_result = {
+            'title': current_feed.title, 
+            'time': cycle_time, 
+            'amount': cycle_items,
+            'amount_total': cycle_items_total
+        }
+        return(cycle_result)
 
     def handle(self, *args, **options):
-        try:
+        try:  # KeyboardInterrupt for Ctrl+C stops
             # execution preparation
             if options['log']:
                 total_start = time.time()
@@ -55,12 +99,16 @@ class Command(BaseCommand):
 
                 # cycle result printing
                 if options['logEach']:
-                    cycle_end = time.time()
-                    cycle_time = round(cycle_end - cycle_start, 2)
-                    print("┣ added " + str(cycle_items) + " feeds in " + str(cycle_time) + "s")
+                    cycle_time = time.time()
+                    cycle_time = round(cycle_time - cycle_start, 2)
+                    Command.print_feed(
+                        title="feeds",
+                        amount=total_items, 
+                        time=cycle_time
+                    )
 
             # caching feedUpdates for feeds stored in DB
-            if options['parseAll'] or options['parseIndex']:
+            if options['parseUpdates']:
                 proxy = False
                 if options["useProxy"]:
                     proxy = requests.get('http://pubproxy.com/api/proxy?https=true&user_agent=true&referer=true').json()['data'][0]["ipPort"]
@@ -75,50 +123,29 @@ class Command(BaseCommand):
                 if options['shuffle']:
                     shuffle(parse_feeds)
 
-                # parsing
-                if options['logBar']:
-                    parse_feeds = tqdm(parse_feeds)
-                for current_feed in parse_feeds:
-                    # cycle preparation
-                    if options['logEach']:
-                        cycle_start = time.time()
-                        cycle_items = 0
+                with ThreadPoolExecutor() as executor:
+                    executor = executor.map(Command.process_feed, parse_feeds)
+                    if options['logBar']:
+                        executor = tqdm(executor, total=len(parse_feeds))
 
-                    feed_present = True
-                    if len(feedUpdate.objects.filter(title=current_feed.title)) == 0:
-                        feed_present = False
-                    
-                    feedUpdate_list = current_feed.parse(proxy)
-
-                    if options["printEmpty"]:
-                        if len(feedUpdate_list) == 0 and current_feed.filter == None:
-                            print(current_feed.href)
-                    
-                    if options['parseNow']:
-                        feedUpdate_list = reversed(feedUpdate_list)
-
-                    for each in feedUpdate_list:
-                        # checking if href is cached
-                        cached = feedUpdate.objects.filter(href=each.href).exists()
-                            
-                        if not cached:
-                            if options['parseNow'] and feed_present:
-                                each.datetime = datetime.now()
-                            each.save()
-
-                            if options['log']:
-                                total_items += 1
-                            if options['logEach']:
-                                cycle_items += 1
-
-                    if options['logEach']:
-                        cycle_end = time.time()
-                        cycle_time = round(cycle_end - cycle_start, 2)
-                        print("┣ added " + current_feed.title + " x" + str(cycle_items) + " in " + str(cycle_time) + "s")
+                    for result in executor:
+                        if options['logEach'] or (options["printEmpty"] and result['amount_total'] == 0):
+                            Command.print_feed(
+                                title=result['title'], 
+                                amount=result['amount'], 
+                                time=result['time']
+                            )
+                        
+                        if options['log']:
+                            total_items += result['amount']
 
             if options['log']:
-                total_end = time.time()
-                total_time = round(total_end - total_start, 2)
-                print("└──── added " + str(total_items) + " in " + str(total_time) + "s")
+                total_time = time.time()
+                total_time = round(total_time - total_start, 2)
+                Command.print_total(
+                    amount=total_items, 
+                    time=total_time
+                )
+                
         except KeyboardInterrupt:
             print('\nKeyboardInterrupt: execution aborted')
